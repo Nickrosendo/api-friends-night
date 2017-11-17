3887c6e11906c89802cd0f695a1d5e8ba37b664c
Nickrosendo <nicolasrosendo@hotmail.com>
2017-11-17 01:38:30 -0200

    Login Post Working

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -10,7 +10,11 @@ module.exports = function(app) {
 
   app.post('/usuarios/usuario', function(req, res) {
     const usuario = req.body;
-    if(usuario){
+    let usuarios = {
+      'email': 'nicolasrosendo@hotmail.com',
+      'senha': '123456'
+    }
+    if(usuario.email == usuarios.email && usuario.senha == usuarios.senha){
       res.status(200).send("usuario cadastrado");
     }else 
       res.status(400).send("falha ao adicinar usuario");
